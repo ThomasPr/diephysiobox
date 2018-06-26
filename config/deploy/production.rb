3bf99c879bc3399221428fb40f6f11c2a3788aa2
Thomas Preißler <thomas@preissler.me>
2018-06-26 22:11:10 +0200

    fix deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server "cfb-physio.de", user: "cfb-physio", roles: %w(app db web)
+set :deploy_to, "/home/cfb-physio/"
 
 
 # role-based syntax
