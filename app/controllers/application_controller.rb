144c4257c869d1ccae97448341df2a03891e769c
Thomas Preißler <thomas@preissler.me>
2019-04-28 01:07:47 +0200

    remove http basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "cfb-physio", password: "awesome"
+  #http_basic_authenticate_with name: "cfb-physio", password: "awesome"
 end
