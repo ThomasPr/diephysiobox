8a5e5224a7ef1a9ccec351d47007a4f555338cad
Thomas Preißler <thomas@preissler.me>
2019-10-19 23:30:27 +0200

    new event page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/home', to: redirect('/')
   get 'sitemap.xml', to: 'home#sitemap', format: 'xml', as: :sitemap
 
+  get 'athletik/kurse', to: 'events#show'
+
   get  'kontakt', to: 'kontakt#new'
   post 'kontakt', to: 'kontakt#create'
 
