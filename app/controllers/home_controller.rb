4986fa08d9c42eae31dfa2e718d85e2785febec2
Thomas Preißler <thomas@preissler.me>
2019-10-20 13:46:38 +0200

    fix urls in the sitemap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
 
   def sitemap
-    @urls = HighVoltage.page_ids.map{ |p| page_url(p) }
+    @urls = (HighVoltage.page_ids - ["datenschutz", "impressum"]).map{ |p| page_url(p) }
     @urls << root_url
     @urls << kontakt_url
+    @urls << athletik_kurse_url
   end
 
 end
