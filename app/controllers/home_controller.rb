fe197000b2c80bc01e4b2755345bcb3686aa02a7
Thomas Preißler <thomas@preissler.me>
2019-10-24 01:05:50 +0200

    remove orphaned athletik_kurse_url from sitemap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
     @urls = (HighVoltage.page_ids - ["datenschutz", "impressum"]).map{ |p| page_url(p) }
     @urls << root_url
     @urls << kontakt_url
-    @urls << athletik_kurse_url
   end
 
 end
