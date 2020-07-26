8fe093dacb362dfe8af8ea327f52d04cd4d360cb
Thomas Preißler <thomas@preissler.me>
2020-07-26 16:37:55 +0200

    finally fix the fontawesome icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-# Add app/assets/fonts to the asset pipeline
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
