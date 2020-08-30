96462dbc6f3c2e63252665975c6e425cb8c4f4fb
Thomas Preißler <thomas@preissler.me>
2020-08-30 20:58:15 +0200

    minor meta-tag improvments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
   def sitemap
     @urls = (HighVoltage.page_ids - ["datenschutz", "impressum"]).map{ |p| page_url(p) }
     @urls << root_url
-    @urls << kontakt_url
   end
 
 end
